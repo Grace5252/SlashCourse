c31c2d785c4e5fe2e886d41f67def2b17fe39f7c
Grace5252 <gracecappella5252@gmail.com>
2025-01-03 17:07:22 -0500

    Basic SlashCourseCharacter w/Movement Added

---
+++ Source/SlashCourse/Public/Characters/SlashCourseCharacter.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SlashCourseCharacter.generated.h"
+
+
+class USpringArmComponent;
+class UCameraComponent;
+class UInputMappingContext;
+class UInputAction;
+class UGroomComponent;
+struct FInputActionValue;
+
+UCLASS()
+class SLASHCOURSE_API ASlashCourseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ASlashCourseCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputMappingContext* SlashCharacterMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* MovementAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* JumpAction;
+
+	void Move(const FInputActionValue& Value);
+	void Look(const FInputActionValue& Value);
+	
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* ViewCamera;
+
+	UPROPERTY(VisibleAnywhere, Category = "Hair")
+	UGroomComponent* Hair;
+
+	UPROPERTY(VisibleAnywhere, Category = "Hair")
+	UGroomComponent* Eyebrows;
+};
