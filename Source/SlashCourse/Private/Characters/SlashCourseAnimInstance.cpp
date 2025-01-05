d17cb2e9dc30344d65975d7c94130857bb7bf285
Grace5252 <gracecappella5252@gmail.com>
2025-01-05 00:37:49 -0500

    Basic Locomotion Animation for Character Class

---
+++ Source/SlashCourse/Private/Characters/SlashCourseAnimInstance.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Characters/SlashCourseAnimInstance.h"
+#include "Characters/SlashCourseCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
+
+void USlashCourseAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+
+	SlashCourseCharacter = Cast<ASlashCourseCharacter>(TryGetPawnOwner());
+	if (SlashCourseCharacter)
+	{
+		SlashCourseCharacterMovement = SlashCourseCharacter->GetCharacterMovement();
+	}
+}
+
+void USlashCourseAnimInstance::NativeUpdateAnimation(float DeltaTime)
+{
+	Super::NativeUpdateAnimation(DeltaTime);
+
+	if (SlashCourseCharacterMovement)
+	{
+		GroundSpeed = UKismetMathLibrary::VSizeXY(SlashCourseCharacterMovement->Velocity);
+	}
+}
---
+++ Source/SlashCourse/Public/Characters/SlashCourseAnimInstance.h
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "SlashCourseAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLASHCOURSE_API USlashCourseAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaTime) override;
+
+	UPROPERTY(BlueprintReadOnly)
+	class ASlashCourseCharacter* SlashCourseCharacter;
+
+	UPROPERTY(BlueprintReadOnly)
+	class UCharacterMovementComponent* SlashCourseCharacterMovement;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Movement")
+	float GroundSpeed;
+};
