38769a5cdf66f1c011997c65684a8e7ef57cc769
Grace5252 <gracecappella5252@gmail.com>
2025-01-17 19:17:12 -0500

    Breakable Actors (Pot A, Pot B)

--- Source/SlashCourse/Public/Interfaces/HitInterface.h
+++ Source/SlashCourse/Public/Interfaces/HitInterface.h
@@ -22,5 +22,7 @@ class SLASHCOURSE_API IHitInterface
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
-	virtual void GetHit(const FVector& ImpactPoint) = 0;
+	//Overridable by blueprint 
+	UFUNCTION(BlueprintNativeEvent)
+	void GetHit(const FVector& ImpactPoint);
 };
